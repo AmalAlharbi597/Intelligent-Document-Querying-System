07c15d0850565bdc67a8f3091d42cc80d62f9a35
AmalAlharbi597 <amalalharbi597@gmail.com>
2025-11-08 17:15:01 +0300

    Update main.tf

--- stack2/main.tf
+++ stack2/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"  # CORRECTED: Must match the region of your Stack 1 resources
+  region = "us-east-1"  
 }
 
 module "bedrock_kb" {
@@ -8,15 +8,16 @@ module "bedrock_kb" {
   knowledge_base_name        = "my-bedrock-kb"
   knowledge_base_description = "Knowledge base connected to Aurora Serverless database"
 
-  aurora_arn        = "arn:aws:rds:us-east-1:704448598553:cluster:my-aurora-serverless" # FILLED
+  aurora_arn        = "arn:aws:rds:us-east-1:704448598553:cluster:my-aurora-serverless" 
   aurora_db_name    = "ISQSDATA"
-  aurora_endpoint   = "my-aurora-serverless.cluster-c89k4w88skvu.us-east-1.rds.amazonaws.com" # FILLED
+  aurora_endpoint   = "my-aurora-serverless.cluster-c89k4w88skvu.us-east-1.rds.amazonaws.com" 
   aurora_table_name = "bedrock_integration.bedrock_kb"
   aurora_primary_key_field = "id"
   aurora_metadata_field = "metadata"
   aurora_text_field = "chunks"
   aurora_verctor_field = "embedding"
   aurora_username   = "dbadmin"
-  aurora_secret_arn = "arn:aws:secretsmanager:us-east-1:704448598553:secret:my-aurora-serverless-sgKrJh" # FILLED
-  s3_bucket_arn = "arn:aws:s3:::bedrock-kb-704448598553" # FILLED
+  aurora_secret_arn = "arn:aws:secretsmanager:us-east-1:704448598553:secret:my-aurora-serverless-sgKrJh"
+  s3_bucket_arn = "arn:aws:s3:::bedrock-kb-704448598553" 
+
 }
